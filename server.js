4e606a477bbb59fd4fb7131bc437df02dc527d6a
Boakye Samuel <boakyesamuel189@gmail.com>
2025-08-06 20:37:46 -0700

    Fix start script for Render deployment

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use((err, req, res, next) => {
 });
 
 // 🚀 Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 
 const startServer = async () => {
   try {
